e677d4bce4a5408943f4cb5cf1efdef2baa1e59a
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-24 12:32:22 -0500

    Migrated to Typescript controls and buttons

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -4,4 +4,3 @@
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
 /// <reference path="angular-material/angular-material.d.ts" />
 /// <reference path="core-js/core-js.d.ts" />
-/// <reference path="angularjs/angular-resource.d.ts" />
