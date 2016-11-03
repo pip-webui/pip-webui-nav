504426c9701f83b0e5abc0c492dd06632c3be763
Sergey Seroukhov <seroukhov@gmail.com>
2016-11-03 09:52:02 -0500

    Fixed _.remove in nav

--- dist/pip-webui-nav.js
+++ dist/pip-webui-nav.js
@@ -533,7 +533,7 @@ var AppBarService = (function () {
     };
     AppBarService.prototype.setShadow = function (breakpoints) {
         var _this = this;
-        this._config.classes = _.remove(this._config.classes, function (c) { return c.startsWith('pip-shadow'); });
+        this._config.classes = _.reject(this._config.classes, function (c) { return c.startsWith('pip-shadow'); });
         if (breakpoints != null) {
             this._config.classes.push('pip-shadow');
             _.each(breakpoints, function (bp) {
@@ -571,7 +571,7 @@ var AppBarService = (function () {
             classes[_i - 0] = arguments[_i];
         }
         _.each(classes, function (c) {
-            _this._config.classes = _.remove(_this._config.classes, function (cc) { return cc == c; });
+            _this._config.classes = _.reject(_this._config.classes, function (cc) { return cc == c; });
         });
         this.sendConfigEvent();
     };
@@ -639,7 +639,7 @@ var AppBarProvider = (function () {
             classes[_i - 0] = arguments[_i];
         }
         _.each(classes, function (c) {
-            _this._config.classes = _.remove(_this._config.classes, function (cc) { return cc == c; });
+            _this._config.classes = _.reject(_this._config.classes, function (cc) { return cc == c; });
         });
     };
     AppBarProvider.prototype.part = function (part, value) {
@@ -2301,7 +2301,7 @@ var SideNavService = (function () {
             classes[_i - 0] = arguments[_i];
         }
         _.each(classes, function (c) {
-            _this._config.classes = _.remove(_this._config.classes, function (cc) { return cc == c; });
+            _this._config.classes = _.reject(_this._config.classes, function (cc) { return cc == c; });
         });
         this.sendConfigEvent();
     };
@@ -2380,7 +2380,7 @@ var SideNavProvider = (function () {
             classes[_i - 0] = arguments[_i];
         }
         _.each(classes, function (c) {
-            _this._config.classes = _.remove(_this._config.classes, function (cc) { return cc == c; });
+            _this._config.classes = _.reject(_this._config.classes, function (cc) { return cc == c; });
         });
     };
     SideNavProvider.prototype.part = function (part, value) {
@@ -2776,6 +2776,18 @@ try {
   module = angular.module('pipNav.Templates', []);
 }
 module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('search/SearchBar.html',
+    '<div class="md-toolbar-tools layout-row" ng-if="vm.enabled"><md-button class="md-icon-button" aria-label="start search" ng-click="vm.onClick()"><md-icon md-svg-icon="icons:search"></md-icon></md-button><input class="pip-search-text flex" type="search" ng-model="vm.search.text" ng-keydown="vm.onKeyDown($event)"><md-button class="md-icon-button" aria-label="clear search" ng-click="vm.clear()"><md-icon md-svg-icon="icons:cross-circle"></md-icon></md-button></div><div class="md-toolbar-tools layout-row layout-align-end-center" ng-if="!vm.enabled"><md-button class="md-icon-button" aria-label="start search" ng-click="vm.enable()"><md-icon md-svg-icon="icons:search"></md-icon></md-button></div>');
+}]);
+})();
+
+(function(module) {
+try {
+  module = angular.module('pipNav.Templates');
+} catch (e) {
+  module = angular.module('pipNav.Templates', []);
+}
+module.run(['$templateCache', function($templateCache) {
   $templateCache.put('sidenav/SideNav.html',
     '<md-sidenav class="md-sidenav-left md-whiteframe-z2 pip-sidenav color-content-bg" md-component-id="pip-sidenav" pip-focused="" ng-transclude=""></md-sidenav>');
 }]);
@@ -2790,18 +2802,6 @@ try {
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('sidenav/StickySideNav.html',
     '<md-sidenav class="md-sidenav-left" md-is-locked-open="sidenavState.isLockedOpen" md-component-id="pip-sticky-sidenav" pip-focused="" ng-transclude=""></md-sidenav>');
-}]);
-})();
-
-(function(module) {
-try {
-  module = angular.module('pipNav.Templates');
-} catch (e) {
-  module = angular.module('pipNav.Templates', []);
-}
-module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('search/SearchBar.html',
-    '<div class="md-toolbar-tools layout-row" ng-if="vm.enabled"><md-button class="md-icon-button" aria-label="start search" ng-click="vm.onClick()"><md-icon md-svg-icon="icons:search"></md-icon></md-button><input class="pip-search-text flex" type="search" ng-model="vm.search.text" ng-keydown="vm.onKeyDown($event)"><md-button class="md-icon-button" aria-label="clear search" ng-click="vm.clear()"><md-icon md-svg-icon="icons:cross-circle"></md-icon></md-button></div><div class="md-toolbar-tools layout-row layout-align-end-center" ng-if="!vm.enabled"><md-button class="md-icon-button" aria-label="start search" ng-click="vm.enable()"><md-icon md-svg-icon="icons:search"></md-icon></md-button></div>');
 }]);
 })();
 
